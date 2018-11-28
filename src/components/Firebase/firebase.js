bf84cb2ba0047ca2a643f055083115cccbef5268
czoekim <c.zoe21@gmail.com>
2018-11-28 17:04:36 -0500

    Fix env and gitignore

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,12 +1,13 @@
 import app from 'firebase/app';
+require('dotenv').config();
 
 const config = {
-  apiKey: keys.env.REACT_APP_API_KEY,
-  authDomain: keys.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: keys.env.REACT_APP_DATABASE_URL,
-  projectId: keys.env.REACT_APP_PROJECT_ID,
-  storageBucket: keys.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: keys.env.REACT_APP_MESSAGING_SENDER_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
 class Firebase {
